5e188d64885eb540b1b1bc84faf540b3aef7b99b
Sky-f1y <marikappanadze@gmail.com>
2025-04-21 15:03:53 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+        <form action="/hello" method="POST">
+            <input name="name">
+            <input type="submit">
+        </form>
+    '''
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form['name']
+    return f"Hello, {name}!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
