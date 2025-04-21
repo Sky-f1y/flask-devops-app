fa1f9fabbae4f637d3da0bbbc463bea4e7c5ef59
Sky-f1y <marikappanadze@gmail.com>
2025-04-21 18:48:31 +0400

    Blue-Green Deployment

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Deploying app..."
+
+# Creating Green deployment folder -- if doesn't exist
+mkdir -p /tmp/green
+
+# Copy latest app version
+cp app.py /tmp/green/
+
+# Simulate switching traffic to green
+ln -sfn /tmp/green /tmp/current
+
+echo "Deployed to /tmp/green and linked to /tmp/current"
